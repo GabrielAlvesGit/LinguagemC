508e2c17d20f8866960e1f2f5da751346bcfb16e
Gabriel Alves <102634725+GabrielAlvesGit@users.noreply.github.com>
2022-09-14 22:04:44 -0300

    Teste

---
+++ Linguagemc.c
@@ -0,0 +1,32 @@
+int main(){
+    char Nome[5];
+    int peso;
+    float altura, imc;
+
+    //printf("Informe seu Nome\n");
+    //scanf("%s", &Nome);
+    //printf("Informe o peso (em kgs):\n\n");
+    //scanf("%d", &peso);
+    //printf("nInforme a altura (em metros):\n\n");
+    //scanf("%f", &altura);
+    
+    //imc = peso / (altura * altura);
+    imc = 2;
+
+    if (imc < 20){
+        printf(" (abaixo do peso)."); 
+    }
+    else if ((imc >= 20) && (imc < 25)){
+        printf(" (peso normal).");
+    }
+    else if ((imc >= 25) && (imc < 30)){
+        printf(" (acima do peso).");
+    }
+    else if ((imc >= 30) && (imc < 34)){
+        printf(" (obeso).");
+    }
+    else{
+		printf(" (muito obeso).");
+    }
+        
+}
